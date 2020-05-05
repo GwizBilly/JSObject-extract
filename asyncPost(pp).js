544546260392b3e27d0178e502ace8f4f53b113d
Guillaume Savoie <f.flat.minor@gmail.com>
2020-05-05 12:09:00 -0400

    created a subsetting routine for results of POST

---
+++ asyncPost(pp).js
@@ -0,0 +1,36 @@
+"use strict";
+const input = require('./output_files/asyncOutput.js'),
+	    inLen = input.length,
+	  columns = Object.keys(input[0]).length;
+
+let out = [];
+
+// prep a 2D array
+for (let i = 0; i < inLen; i++) {
+	out[i] = [];
+}
+
+// create header for each column
+for (let j = 0; j < columns; j++) {
+	out[0][j] = input[0][j];
+}
+//console.log(out[0]);
+//console.log(input[1]["object"]);
+// perform subsetting
+for (let i = 0; i < inLen - 1; i++) {
+	for (let j = 0; j < columns - 2; j++) {
+		out[i + 1][j] = input[i + 1]["object"][input[0][j]];
+	}
+}
+
+for (let i = 0; i < inLen - 1; i++) {
+	for (let j = columns - 2; j < columns; j++) {
+		// last for loop to concatenate al geographies into one string
+		let len = input[i + 1]["object"].dimension[0].member.length;
+		for (let k = 0; k < len; k++) {
+			out[i + 1][j] = input[i + 1]["object"].dimension[0].member[k].memberNameEn;
+		}
+	}
+}
+
+console.log(out[1]);
