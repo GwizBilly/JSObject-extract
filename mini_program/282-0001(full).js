ae65ee691648c54e4fb45b2672ecbcdcf37374c9
Guillaume Savoie <46363174+GwizBilly@users.noreply.github.com>
2020-05-06 16:02:48 -0400

    Update 282-0001(full).js

--- mini_program/282-0001(full).js
+++ mini_program/282-0001(full).js
@@ -1,2 +1,3 @@
+// all this does is create a view on one of the {} object that returns from asyncPost.js
 const LFS2820001 = require("../output_files/asyncOutput.js");
 console.log(JSON.stringify(LFS2820001[1]));
